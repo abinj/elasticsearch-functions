492660488ea920c75f0029c9eaf6463fa688bbd9
abinj <abinj@dexlock.com>
2018-03-24 19:21:36 +0530

    First commit

---
+++ src/main/java/com/abinj/elasticsearchfunctions/application/MainApplication.java
@@ -0,0 +1,9 @@
+package com.abinj.elasticsearchfunctions.application;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
